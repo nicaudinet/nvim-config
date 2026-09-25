8557b723b29300b5367dd811dae34fe5778fb9a6
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2026-09-25 13:30:33 +0200

    client:supports_method API change

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -18,31 +18,33 @@ local opts = {
 
 -- Set common key bindings
 vim.api.nvim_create_autocmd("LspAttach", {
+
   callback = function(args)
+
     local client = vim.lsp.get_client_by_id(args.data.client_id)
 
     -- Rename a variable (leader-lsp-Rename)
-    if client.supports_method("textDocument/rename") then
+    if client:supports_method("textDocument/rename") then
       vim.keymap.set("n", "<Leader>lR", vim.lsp.buf.rename, opts)
     end
 
     -- Go to definition (leader-lsp-parent)
-    if client.supports_method("textDocument/definition") then
+    if client:supports_method("textDocument/definition") then
       vim.keymap.set("n", "<Leader>lp", vim.lsp.buf.definition, opts)
     end
 
     -- Go to the type definition (leader-lsp-type)
-    if client.supports_method("textDocument/typeDefinition") then
+    if client:supports_method("textDocument/typeDefinition") then
       vim.keymap.set("n", "<Leader>lt", vim.lsp.buf.type_definition, opts)
     end
 
     -- Format the current buffer with the LSP (leader-lsp-format)
-    if client.supports_method("textDocument/formatting") then
+    if client:supports_method("textDocument/formatting") then
       vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format, opts)
     end
 
     -- Code action (leader-lsp-action)
-    if client.supports_method("textDocument/codeAction") then
+    if client:supports_method("textDocument/codeAction") then
       vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action, opts)
     end
 
@@ -56,13 +58,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local telescope = require("telescope.builtin")
 
     -- Show references for word under cursor (leader-lsp-references)
-    if client.supports_method("textDocument/references") then
+    if client:supports_method("textDocument/references") then
       vim.keymap.set("n", "<Leader>lr", telescope.lsp_references, opts)
     end
 
     -- Show all diagnostics (ledaer-lsp-Diagnostics)
-    if client.supports_method("textDocument/publishDiagnostics") then
+    if client:supports_method("textDocument/publishDiagnostics") then
       vim.keymap.set("n", "<Leader>lD", telescope.diagnostics, opts)
     end
+
   end,
+
 })
